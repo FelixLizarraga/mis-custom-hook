460a38e63591997ce64b64afcf409fb58472095f
Felix Lizarraga <felixlizarraga031@gmail.com>
2022-05-07 14:46:33 -0700

    Primer commit

---
+++ useCounter.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+
+export const useCounter = (inicialState = 1) => {
+  
+    const [state, setState] = useState(inicialState)
+
+    const increment = () => {
+        setState(state + 1);
+    }
+
+    const decrement = () => {
+        setState(state - 1);
+    }
+
+    const reset = () => {
+        setState(inicialState);
+    }
+
+    return {
+        state,
+        increment,
+        decrement,
+        reset
+    };
+}
